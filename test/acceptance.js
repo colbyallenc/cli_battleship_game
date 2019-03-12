c5c26573ff829e4bbf5928dd86b33570260abe55
colby allen <colbyallenc@gmail.com>
2019-03-12 17:03:06 -0400

    feat(tests): initial acceptance test/cmd.js file created and working

--- test/acceptance.js
+++ test/acceptance.js
@@ -1,3 +1,18 @@
-// the game
-// does the executable file even run
-//start with a file and break it out later
+const { expect } = require('chai');
+const { cmd } = require('./cmd');
+
+describe('Battleship', () => {
+  it('Welcomes you to the game @acceptance', async () => {
+    const response = await cmd.execute(
+      './src/gameBoard.js',
+    );
+
+    //shows an empty board --> fail
+    //add board --> pass
+    //board.js unit test
+        
+    expect(response).to.equal(
+      'you ordered a pizza with:\n  - peppers\n  - gouda cheese',
+    );
+  });
+});
