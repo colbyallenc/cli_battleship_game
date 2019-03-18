da80940c47bd22676c5126cb2ff809a81dfa4d4d
colby allen <colbyallenc@gmail.com>
2019-03-18 14:51:31 -0400

    Acceptance test task list

--- test/acceptance.js
+++ test/acceptance.js
@@ -4,9 +4,7 @@ const { cmd } = require('./cmd');
 
 describe('Battleship Game Play', () => {
   xit('@acceptance: Displays a Game Board', async () => {
-    const response = await cmd.execute(
-      './bin/game.js',
-    );
+    const response = await cmd.execute('./bin/game.js');
     const result = response.trim().split(EOL);
     const expected = [
       '- 1 2 3',
@@ -14,14 +12,11 @@ describe('Battleship Game Play', () => {
       'B ~ ~ ~',
       'C ~ ~ ~',
     ];
-
     expect((result)).to.deep.equal(expected);
   });
 
   xit('@acceptance: Places a ship on the Game Board', async () => {
-    const response = await cmd.execute(
-      './bin/game.js',
-    );
+    const response = await cmd.execute('./bin/game.js');
     const result = response.trim().split(EOL);
     const expected = [
       '- 1 2 3',
@@ -29,7 +24,26 @@ describe('Battleship Game Play', () => {
       'B ~ ~ ~',
       'C ~ D D',
     ];
-
     expect((result)).to.deep.equal(expected);
+  });
+  xit('Cpu generates board', () => {
+  });
+  xit('Display boards', () => {
+  });
+  xit('Take a shot at opponents board', () => {
+  });
+  xit('Update board when opponent ship is hit', () => {
+  });
+  xit('Opponent takes a shot at player board', () => {
+
+  });
+  xit('Display board when player board is hit', () => {
+
+  });
+  xit('If length of board is hit, sink ship', () => {
+
+  });
+  xit('If all of the boats are sunk, win game', () => {
+
   });
 });
