1decbdd1029f09ceb94c6c532369c25c116cc62e
colby allen <colbyallenc@gmail.com>
2019-03-20 22:43:29 -0400

    feat: Added board tasks to board test

--- bin/board.js
+++ bin/board.js
@@ -1,10 +1,29 @@
-// class Board {
-//   constructor() {
-//     this.small = null;
-//     this.medium = null;
-//     this.large = null;
-//   }
-// }
+// TODO: keep this list in the board
+let board = new Board();
+board.placeShip('D', 'A', 1);
+let printed = board.printBoard();
+console.log(printed);
+   // board should look like:
+    /* 
+    - 1 2 3
+    A D D ~
+    B ~ ~ ~
+    C ~ ~ ~
+    */
 
-// const newBoard = new Board();
-// console.log(newBoard);
+const ships = [
+  { name: 'D', row: 'A', col: 1 },
+];
+
+function printShip(row, col) {
+  let foundShip = [];
+  for (let i = 0; i < ships.length; i++) {
+    if (ships[i].row === row && ships[i].col === col) {
+      foundShip = ships[i].name;
+    }
+  }
+}
+
+function placeShip(name, row, col) {
+  ships.push({ name: name, row: row, col: col });
+}
