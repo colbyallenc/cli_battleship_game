05807c2889b6630ca5b73a1d5135f71d5cd02641
colby allen <colbyallenc@gmail.com>
2019-03-20 22:46:26 -0400

    feat: geame test added

---
+++ test/game_test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const { Game } = require('../bin/game.js');
+
+
+Describe('', () => {
+    Describe('', () => {
+        
+    });
+    Describe('Results of the Game', () => {
+        xit('Displays when Player Loses', () => {
+            expect(());
+        });
+        xit('Displays when Player Wins', () => {
+            expect(());
+        });
+    });
+});
