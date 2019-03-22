2ca053e0df4514fe8ab8cb243fb2f345a8ecf517
colby allen <colbyallenc@gmail.com>
2019-03-22 06:28:05 -0400

    feat: board queries ships array for ships at position x

--- bin/board.js
+++ bin/board.js
@@ -1,29 +1,36 @@
-// TODO: keep this list in the board
-let board = new Board();
-board.placeShip('D', 'A', 1);
-let printed = board.printBoard();
-console.log(printed);
-   // board should look like:
-    /* 
-    - 1 2 3
-    A D D ~
-    B ~ ~ ~
-    C ~ ~ ~
-    */
-
-const ships = [
-  { name: 'D', row: 'A', col: 1 },
+let ships = [
+  { name: 'D', row: 'A', col: 3 },
+  { name: 'D', row: 'B', col: 3 },
 ];
 
-function printShip(row, col) {
-  let foundShip = [];
+function getBoard() {
+  let board = [
+    '- 1 2 3',
+    `A ${getShipAt('A', 1)} ${getShipAt('A', 2)} ${getShipAt('A', 3)}`,
+    `B ${getShipAt('B', 1)} ${getShipAt('B', 2)} ${getShipAt('B', 3)}`,
+    `C ${getShipAt('C', 1)} ${getShipAt('C', 2)} ${getShipAt('C', 3)}`,
+  ].join('\n');
+
+  return board;
+}
+//stores ship in ships array
+function storeShips(name, row, col) {
+  ships.push({ name: name, row:row, col: col});
+  return ships;
+}
+
+
+function getShipAt(row, col) {
+  let foundShip = '~';
   for (let i = 0; i < ships.length; i++) {
     if (ships[i].row === row && ships[i].col === col) {
       foundShip = ships[i].name;
     }
   }
+  return foundShip;
 }
 
-function placeShip(name, row, col) {
-  ships.push({ name: name, row: row, col: col });
-}
+module.exports = {
+  getBoard,
+  storeShips,
+};
--- bin/game.js
+++ bin/game.js
@@ -1,42 +1,74 @@
 #! /usr/local/bin/node 
 //why doesnt env locate node?
-class Game {
-  constructor() {
+
+let ships = [
+  { name: 'D', row: 'A', col: 3, hits: },
+  { name: 'D', row: 'B', col: 3, hits: }
+];
+//  //stores ship in ships array
+// function storeShips(name, row, col){
+//   ships.push({ name: name, row:row, col: col})
+//   return ships
+// }
+// //queries ships array for ship at current position
+// function getShipAt(row, col) {
+// let foundShip = '~'
+//   for(let i = 0; i < ships.length; i++){
+//     if(ships[i].row === row && ships[i].col === col){
+//       foundShip = ships[i].name
+//     }
+//   }
+//   return foundShip
+// }
+// console.log(storeShips("D", "A", 4));
+// console.log(board)
+
+// module.exports = { Game };
+
+
+// Class attempt
+class Board {
+  constructor(){
     this.board = [
-      '- 1 2 3',
-      'A ~ ~ ~',
-      'B ~ ~ ~',
-      'C ~ ~ ~',
-    ].join('\n');
-    this.shipPosition = [];
-  }
+        '- 1 2 3',
+        `A ${getShipAt('A', 1)} ${getShipAt('A', 2)} ${getShipAt('A', 3)}`,
+        `B ${getShipAt('B', 1)} ${getShipAt('B', 2)} ${getShipAt('B', 3)}`,
+        `C ${getShipAt('C', 1)} ${getShipAt('C', 2)} ${getShipAt('C', 3)}`,
+      ].join('\n');
+    this.ships = [];
 
-  start() {
-    return this.board;
   }
-
-  placeShip() {
-    [
-      '- 1 2 3',
-      `A ${getShipAt('A', 1)} ${getShipAt('A', 2)} ${getShipAt('A', 3)}`,
-      `B ${getShipAt('B', 1)} ${getShipAt('B', 2)} ${getShipAt('B', 3)}`,
-      `C ${getShipAt('C', 1)} ${getShipAt('C', 2)} ${getShipAt('C', 3)}`,
-    ].join('\n');
-    return this.board;
+ //stores ship in ships array
+  storeShips(name, row, col){
+    this.ships.push({ name: name, row:row, col: col})
+    return this.ships
   }
-
-  queryBoard() {
-    this.placeShip();
-    for (let i = 0; i < this.board.length; i++) {
-      if (this.board[i] === 'D') {
-        this.shipPosition.push(i);
+  // queries ships array for ship at current position
+  getShipAt(row, col) {
+  let foundShip = '~'
+    for(let i = 0; i < this.ships.length; i++){
+      if(this.ships[i].row === row && this.ships[i].col === col){
+        foundShip = this.ships[i].name
       }
     }
-    return this.shipPosition;
+    return foundShip
+  }
+
+  drawBoard(){
+    this.board = [
+        '- 1 2 3',
+        `A ${this.getShipAt('A', 1)} ${this.getShipAt('A', 2)} ${this.getShipAt('A', 3)}`,
+        `B ${this.getShipAt('B', 1)} ${this.getShipAt('B', 2)} ${this.getShipAt('B', 3)}`,
+        `C ${this.getShipAt('C', 1)} ${this.getShipAt('C', 2)} ${this.getShipAt('C', 3)}`,
+      ].join('\n');
   }
 }
 
-module.exports = { Game };
+let board = new Board();
+board.storeShips("D", "A", 4);
+console.log(board);
+// placeShip("S", "C", 1);
+// board.printShips()
 
 // const destroyer = new Ship('C2', 'C3');
 // const game = new Game();
